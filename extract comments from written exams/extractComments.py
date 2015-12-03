8e8a3aac342d0000aa04de2d870d14c834ff0e22
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2015-12-03 08:22:17 +0100

    Code cleanup according to pep8.

--- extract comments from written exams/extractComments.py
+++ extract comments from written exams/extractComments.py
@@ -10,31 +10,31 @@
 # Date: 2014-01-15
 
 # Load dependencies
-import sys, os.path
+import sys
 import os
 import re
-from pdfminer.psparser import PSKeyword, PSLiteral, LIT
 from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument, PDFNoOutlines
-from pdfminer.pdftypes import PDFObjectNotFound, PDFValueError
-from pdfminer.pdftypes import PDFStream, PDFObjRef, resolve1, stream_value
-from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdftypes import PDFObjectNotFound
 
-# dumpcomments
+
+# dump_comments
 # Analyzes an open pdf file and extracting comments from it.
 # It is tested with comments inserted with PDF X-Change Viewer.
-def dumpcomments(doc):
+def dump_comments(doc):
     comments = []
     visited = set()
     for xref in doc.xrefs:
         # Iterate over all objects in the pdf file.
         for objid in xref.get_objids():
-            if objid in visited: continue
+            if objid in visited:
+                continue
             visited.add(objid)
             try:
                 obj = doc.getobj(objid)
-                if obj is None: continue
-                if(isinstance(obj, dict)):
+                if obj is None:
+                    continue
+                if isinstance(obj, dict):
                     try:
                         # Comments inserted with PDF X-Change Viewer have these
                         # two values, if they are not present the object is not
@@ -49,63 +49,71 @@ def dumpcomments(doc):
     return comments
 
 
-# getCommentsInPdfFile
-# Loads a pdf file and extracts comments from that file.
-def getCommentsInPdfFile(filename):
+def get_comments_in_pdf_file(filename):
+    """
+
+    :param filename: Name of pdf file to analyse.
+    :return: List of comments found in the pdf file.
+    """
     fp = file(filename, 'rb')
     parser = PDFParser(fp)
     doc = PDFDocument(parser)
-    comments = dumpcomments(doc)
+    comments = dump_comments(doc)
     fp.close()
     return comments
+
 
 # main function that iterates through all subdirs and analyzes all files in
 # each subdir.
 # Extracted comments are stored in the file "statistics".
 def main():
-    outputFile = open('statistics', 'w')
+    output_file = open('statistics', 'w')
 
     header = "ID\tExercise\tPoint\tComment"
     print(header)
-    outputFile.write(header + "\n")
-    subdirs = getListOfSubdirectories('./')
-    for subdir in subdirs:
+    output_file.write(header + "\n")
+    sub_directories = get_list_of_subdirectories('./')
+    for subdir in sub_directories:
         print("\n\n=======================================")
         print("Subdir: %s" % subdir)
         assignment = subdir[2:]
-        tempDir = subdir + '/'
-        files = getFilesInDirectory(tempDir)
-        for file in files:
+        temp_dir = subdir + '/'
+        files = get_files_in_directory(temp_dir)
+        for filename in files:
+            full_filename = assignment + "/" + filename
             try:
-                fullFileName = assignment + "/" + file
-                comments = getCommentsInPdfFile(fullFileName)
+                comments = get_comments_in_pdf_file(full_filename)
                 for comment in comments:
-                    opgave, point, comment = splitComment(comment)
-                    observation = "%s\t%s\t%s\t%s" % (assignment, opgave, point, comment.replace('\n', ' ').replace('\r', ' '))
+                    opgave, point, comment = split_comment(comment)
+                    observation = "%s\t%s\t%s\t%s" % (assignment, opgave,
+                                                      point, comment.replace('\n', ' ').replace('\r', ' '))
                     print(observation)
-                    outputFile.write(observation + "\n")
+                    output_file.write(observation + "\n")
             except:
-                print("Error analyzing file: %s" % fullFileName)
+                print("Error analyzing file: %s" % full_filename)
 
-    outputFile.close()
+    output_file.close()
 
     pass
 
-# Function for splitting comments in three parts:
-# * exerciseid
-# * awarded points
-# * comments.
-# This is returned as a tuple.
-def splitComment(comment):
+
+def split_comment(comment):
+    """
+    Function for splitting comments in three parts:
+
+    :param comment: The comment text string that should be analyzed.
+    :return: Tuple with the values, exerciseid, number of awarded points and a text string with comments.
+    """
     val = re.match("([0123456789]+[abcde]) (\d+)\s*(.*)", comment)
-    if(val):
+    if val:
         exercise = val.group(1)
         point = val.group(2)
         comment = val.group(3)
         return (exercise, point, comment)
     return ("NA", "NA", comment)
 
-def getListOfSubdirectories(path):
+
+def get_list_of_subdirectories(path):
     dirs = []
     files_in_dir = os.listdir(path)
     for file_in_dir in files_in_dir:
@@ -113,12 +121,14 @@ def getListOfSubdirectories(path):
             dirs.append(path + file_in_dir)
     return dirs
 
-def getFilesInDirectory(path):
+
+def get_files_in_directory(path):
     files = []
     files_in_dir = os.listdir(path)
     for file_in_dir in files_in_dir:
         if os.path.isfile(path + file_in_dir):
             files.append(file_in_dir)
     return files
+
 
 main()
